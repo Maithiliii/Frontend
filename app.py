233ac28b31cbcced8ba580823d02d41de16794bd
Maithili Naik <maithilinaik0910@gmail.com>
2025-05-09 10:45:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template
 import os
 import numpy as np
 import cv2
@@ -8,23 +8,40 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# Load single model for now (used for all organs)
-cae_model = tf.keras.models.load_model("cae_model.h5")
+cae_model = tf.keras.models.load_model("brainkidney.h5", compile=False)
+classifier_model = tf.keras.models.load_model("best_model.h5", compile=False)
+class_labels = ['glioma', 'meningioma', 'notumor', 'pituitary']
 
 def preprocess_image(image_path, target_size=(256, 256)):
     img = cv2.imread(image_path)
-    img = cv2.resize(img, target_size) / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img
+    img = cv2.resize(img, target_size).astype("float32") / 255.0
+    return np.expand_dims(img, axis=0)
 
 def reconstruct_image(image_path):
-    img = preprocess_image(image_path)
-    reconstructed_img = cae_model.predict(img)[0]
-    reconstructed_img = (reconstructed_img * 255).astype(np.uint8)
-    reconstructed_img = reconstructed_img.squeeze()
+    img = cv2.imread(image_path)
+
+    img = cv2.resize(img, (256, 256)).astype("float32") / 255.0  
+
+    input_img = np.expand_dims(img, axis=0)
+
+    output_img = cae_model.predict(input_img)[0]
+
+    output_img = np.clip(output_img * 255, 0, 255).astype(np.uint8)
+
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
+
     reconstructed_path = os.path.join(app.config["UPLOAD_FOLDER"], "reconstructed.png")
-    cv2.imwrite(reconstructed_path, reconstructed_img)
+    cv2.imwrite(reconstructed_path, output_img)
+
     return reconstructed_path
+
+def classify_tumor(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, (240, 240))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype("float32") / 255.0
+    img = np.expand_dims(img, axis=0)
+    pred = classifier_model.predict(img)
+    return class_labels[np.argmax(pred)]
 
 @app.route("/")
 def home():
@@ -46,10 +63,13 @@ def process(organ):
     file.save(filepath)
 
     denoised_path = reconstruct_image(filepath)
+    
+    predicted_class = classify_tumor(filepath)
 
     return jsonify({
         "original": f"/static/uploads/{file.filename}",
-        "denoised": f"/{denoised_path}"
+        "denoised": f"/static/uploads/reconstructed.png",  
+        "prediction": predicted_class
     })
 
 if __name__ == "__main__":
